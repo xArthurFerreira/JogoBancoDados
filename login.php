a16d848a0ecee3ef02ed03b4c4a11816b45df1bd
xArthurFerreira <141787340+xArthurFerreira@users.noreply.github.com>
2024-04-07 23:54:27 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+
+$host = "localhost"; // Host name 
+$db_username = "XXXX_user"; // Mysql username 
+$db_password = "XXXXX"; // Mysql password 
+$db_name = "XXXXX_db"; // Database name 
+$db_table = "scores2"; // Table name
+
+?>
